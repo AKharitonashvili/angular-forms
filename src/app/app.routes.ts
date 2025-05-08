ebe78255164674acb14cf2f9c4c5057b1764cfa3
AKharitonashvili <sandroxari@gmail.com>
2025-05-08 17:48:52 +0400

    componetns

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/forms', pathMatch: 'full' },
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
   {
     path: 'forms',
     loadChildren: () =>
       import('./forms/forms.routes').then((m) => m.FORMS_ROUTES),
+  },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./pages/posts/posts.routes').then((m) => m.POSTS_ROUTES),
   },
 ];
